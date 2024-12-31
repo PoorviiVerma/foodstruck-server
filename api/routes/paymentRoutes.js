5d52a4b47c18a0739486f268058a1fac3cba46e1
Poorvi Verma <176572307+PoorviiVerma@users.noreply.github.com>
2024-12-31 18:28:08 +0530

    first commit

---
+++ api/routes/paymentRoutes.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const Payment = require('../models/Payments');
+const verifyToken = require('../middleware/verifyToken');
+const router = express.Router();
+const Carts = require('../models/Carts');
+const ObjectId = mongoose.Types.ObjectId;
+
+
+// post payment info to db
+router.post('/', verifyToken , async(req, res) => {
+    const payment = req.body;
+    try {
+        const paymentRequest = await Payment.create(payment);
+        // delete cart after payment
+        const cartIds = payment.cartItems.map(id => new ObjectId(id));
+        const deleteCartRequest = await Carts.deleteMany({_id: {$in: cartIds}})
+        res.status(200).json({paymentRequest, deleteCartRequest})
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+})
+router.get('/', verifyToken, async(req,res) => {
+    const email = require.query.email;
+    const query = {email: email}
+    try {
+        const decodedEmail = req.decoded.email;
+        if (email !== decodedEmail) {
+            res.status(403).json({message: "Forbiden Access"})
+        }
+        const result = await Payment.find(query).sort({createdAt: -1}).exec();
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+});
+
+// get all payments by user
+router.get('/all', async(req, res) => {
+    try {
+        const payments = await Payment.find({}).sort({createdAt: -1}).exec();
+        res.status(200).json(payments)
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+})
+
+// confirm payment status
+router.patch('/:id', async(req, res) => {
+    const payId = req.params.id;
+    const {status} = req.body;
+    try {
+        const updatedStatus = await Payment.findByIdAndUpdate(payId, {status: "confirmed"},
+            {new: true, runValidators: true}
+        );
+
+        if(!updatedStatus){
+            return res.status(404).json({message: "Payment not found"});
+        }
+        res.status(200).json(updatedStatus)
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+})
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port =  process.env.PORT || 6001;
+const mongoose = require('mongoose');
+require('dotenv').config()
+const jwt = require('jsonwebtoken');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+//  mongodb config
+mongoose
+.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@demo-foodi-client.va8bh.mongodb.net/demo-foodi-client?retryWrites=true&w=majority&appName=demo-foodi-client`
+)
+.then(
+  console.log("MongoDB Connected Successfully")
+).catch((error) => console.log("Error connecting to MongoDB", error)
+);
+// require('crypto').randomBytes(64).toString('hex')
+
+
+
+ // jwt authentication
+ app.post('/jwt', async(req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: '1hr'
+  })
+  res.send({token});
+})
+// ctrl shift F
+
+
+
+//   import routes here
+const menuRoutes = require('./api/routes/menuRoutes');
+app.use('/menu', menuRoutes)
+
+const cartRoutes = require('./api/routes/cartRoutes');
+app.use('/carts', cartRoutes);
+
+const userRoutes = require('./api/routes/userRoutes');
+app.use('/users', userRoutes);
+
+const paymentRoutes = require('./api/routes/paymentRoutes')
+app.use('/payments', paymentRoutes);
+
+const adminStats = require('./api/routes/adminStats')
+app.use('/adminStats', adminStats);
+
+
+const orderStats = require('./api/routes/orderStats')
+app.use('/orderStats', orderStats);
+
+// stripe payment routes
+app.post("/create-payment-intent", async (req, res) => {
+  const { price } = req.body;
+  const amount = parseInt(price*100);
+
+// Create a PaymentIntent with the order amount and currency
+const paymentIntent = await stripe.paymentIntents.create({
+  amount: amount,
+  currency: "INR",
+  payment_method_types: ["card"]
+});
+
+res.send({
+  clientSecret: paymentIntent.client_secret,
+});
+});
+app.get('/', (req, res) => {
+  res.send('Hello, Poorvi!')
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
